ae7ec341f365e65e027369f28c5f66eb0b1b41c2
Banerjee <rbanerjee001@ets.org>
2025-10-30 04:29:29 -0400

    initial commit

---
+++ tests/UIBasics.spec.js
@@ -0,0 +1,19 @@
+const{test} = require('@playwright/test')
+const { AsyncLocalStorage } = require('async_hooks')
+test ('first PW tets',async function()
+{
+    //playwright code
+    //step 1
+    await
+    //step 2
+
+});
+test ('second PW tets',async ()=>
+{
+    //playwright code
+    // in java script ()=> can be used to create a function without name to keep the code lighter(anonymous function)
+    //step 1
+    await
+    //step 2
+
+});
