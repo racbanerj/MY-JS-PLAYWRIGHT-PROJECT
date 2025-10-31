b5c5e1c8face20e93b29491b50bc63e95eb9312d
Banerjee <rbanerjee001@ets.org>
2025-10-31 09:28:35 -0400

    changes till 31st oct

--- playwright.config.js
+++ playwright.config.js
@@ -13,69 +13,18 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+                             //export keyword is used to export the config details throughout all the modules
+  testDir: './tests',       //to fetch tests from this folder
+  timeout: 40000,         //declaring default timeout seconds
+  expect:{
+    timeout: 50000
+    ,       //declaring timeout explicitly for assertions
   },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  reporter: 'html',
+  use:
+  {
+    browserName : 'chromium'    , //which browser to use
+    headless :  false      //instead of typing in command lien to run in head mode , mention in confi file 
+  }
 });
 
--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,19 +1,32 @@
-const{test} = require('@playwright/test')
+const{test,expect} = require('@playwright/test')   //imprting test and expect annotations
 const { AsyncLocalStorage } = require('async_hooks')
-test ('first PW tets',async function()
+test ('browser context playwright test',async function({browser})
 {
     //playwright code
-    //step 1
-    await
+    
+    const context =  await browser.newContext();  //newContext method helps to open a new instance of browser
+                                                  //inside newContext method we can add parameters to inject cookies with which the brpwser will open
+    const page= await context.newPage();          //newPage method creates new page after opening the browser 
+    await page.goto("https://saucelabs.com/");
+    //browser here is fixture/global default variable in playwright
+    
     //step 2
 
 });
-test ('second PW tets',async ()=>
+test.only ('page playwright test',async ({browser,page})=>
 {
     //playwright code
+    //test.only is used when you want to run only that test function form that file
     // in java script ()=> can be used to create a function without name to keep the code lighter(anonymous function)
-    //step 1
-    await
-    //step 2
+    //newContext and newPage method is default, so we we declare the fixture called "page" inside the funcion parameter, it will work as same as the above function. "Page" ficture is a default variable of playwrite which contains the mechanism of newcontext and newPgae
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log (await page.title());
+    //await  expect(page).toHaveTitle("Google");
+    await page.locator('input#username').fill("rachana");
+    await page.locator('input#password').fill("learning");
+    await page.locator('#signInBtn').click();;
+    console.log(await page.locator("[style='display: block;']").textContent());
+    await expect(page.locator("[style='display: block;']")).toContainText('Incorrect');
 
 });
