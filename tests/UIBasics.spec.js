4f8510c0175c356dc7871bc32139f611c63824de
Banerjee <rbanerjee001@ets.org>
2025-11-02 15:03:20 -0500

    commit of 2nd nov 2025

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -13,20 +13,100 @@ test ('browser context playwright test',async function({browser})
     //step 2
 
 });
-test.only ('page playwright test',async ({browser,page})=>
+test ('page playwright test',async ({browser,page})=>
 {
     //playwright code
     //test.only is used when you want to run only that test function form that file
     // in java script ()=> can be used to create a function without name to keep the code lighter(anonymous function)
     //newContext and newPage method is default, so we we declare the fixture called "page" inside the funcion parameter, it will work as same as the above function. "Page" ficture is a default variable of playwrite which contains the mechanism of newcontext and newPgae
 
+   
+    
+    const userName= page.locator('input#username');  //storing the username element in a global variable which can be used multiple times throughout the class
+    const signinBtn= page.locator('#signInBtn');
+    const cardTitles= page.locator(".card-body a");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log (await page.title());
-    //await  expect(page).toHaveTitle("Google");
-    await page.locator('input#username').fill("rachana");
+    //await  expect(page).toHaveTitle("Google"); //expect() is for verifying
+    await userName.fill("rachana");  //fill() is for typing ; //typing icorrect username now to generate error message test case
     await page.locator('input#password').fill("learning");
-    await page.locator('#signInBtn').click();;
-    console.log(await page.locator("[style='display: block;']").textContent());
-    await expect(page.locator("[style='display: block;']")).toContainText('Incorrect');
+    await signinBtn.click();
+    console.log(await page.locator("[style='display: block;']").textContent());  
+    await expect(page.locator("[style='display: block;']")).toContainText('Incorrect'); // validating error text message which is displayed for few seconds
+    await userName.fill("");  //fill("") is used to erase the existing texts written on the textbox already
+    await userName.fill("rahulshettyacademy"); //typing correct username this time
+    await signinBtn.click(); //user should be able to login successfully now
+    console.log(await cardTitles.first().textContent()); //locator used traversing from parent to child; first() method used to select the first matching css as the css has 4 matching element .
+    console.log(await cardTitles.nth(1).textContent());  //nth() is used to select the required matching element csss from the index(here we need 2nd element so used index 1)
+    console.log(await cardTitles.last().textContent());  //first() method used to select the last matching css .
+    const allTitles= await cardTitles.allTextContents(); //allTestContents() method is used to fetch array of multiple element texts of all the matching elements of the css.npx playwright
+    console .log(allTitles);
+
+
+
+});
+test ('dropdown checkbox web popup tests',async ({browser,page})=>
+
+{
+    const userName= page.locator('input#username');  //storing the username element in a global variable which can be used multiple times throughout the class
+    const signinBtn= page.locator('#signInBtn');
+    const password = page.locator('input#password');
+    const cardTitles= page.locator(".card-body a");
+    const dropdown = page.locator("select.form-control");
+    const userTypCheckbox =  page.locator(".checkmark"); //admin or user
+    const doclink= page.locator ("[href*=documents-request]");
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  
+    //await  expect(page).toHaveTitle("Google"); //expect() is for verifying
+    await userName.fill("rachana");  //fill() is for typing ; //typing icorrect username now to generate error message test case
+    await password.fill("learning");
+    await dropdown.selectOption("consult");
+    await userTypCheckbox.last().click();
+    await page.locator("div #okayBtn").click();
+    console.log(await  page.locator(".checkmark").last().isChecked()); //isChecked() returns boolean value and it is used to print if it is checked(true or false). This is not for assertion.
+    //await page.pause(); //to pause and check visually what option was selected
+    await expect (userTypCheckbox.last()).toBeChecked();//to assert that the specific checkbox is selected
+   
+    await page.locator("#terms").click();
+    await     expect(  page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck(); //uncheck() is used to uncheck the checkboxes explicitly in playwright
+     expect ( await page.locator("#terms").isChecked()).toBeFalsy(); // tobefalsy() asserts that the ischecked() results is false.This way we can assert that a checkbox is unchecked.
+                                                                     // await is mentioned inside the expect(), not outside like other lines. why? 
+                                                                     //because await is only required where any action is happening. Here ischecked() is an action inside the expect() ,.. expect(ischecked())..hence await is required
+                                                                     //in the above line  action is done outside expect()... expect().tobechecked()
+ //in Playwright, there is no method called isChecked() directly on the page object, but it does exist on Locator. so to use ischecked() we have to use it with the locator, not with the object
+    await expect (doclink).toHaveAttribute("class","blinkingText"); //toHaveAttribute() is used to asert that a blinking text is available with the class "blinking text"
+
+
+});
+
+test.only ('child windows handling',async ({browser})=>
+{
+    //when a new page is opened from a yper link , we have to feed that information in a new page variable
+    //in the below lines of code , we are using two lined asynchronusly i.e. waiting for a new page and clicking on the hyper link to get the new page is happening parallelly 
+    // two lines of code is working as an array and returning a new page to the [newPage] variable in array format
+    //child pages are always returned in array format as multiple pages can be opened 
+
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const doclink= page.locator ("[href*=documents-request]");
+  
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    const [newPage]=await Promise.all(
+        [
+           context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+           doclink.click(),
+        
+        ])//new page is opened
+        const newPageheading =  newPage.locator(".red");
+        const text= await newPageheading.textContent();
+        console.log(text);
+        const arrayText= text.split("@"); //splitting the retreived text by the character @
+        const domain=arrayText[1].split(" ") [0]; //taking the 1nth index and splitting that by space and storing the 0th index from the retreived value into domain variable
+        console.log(domain);
+        await page.locator("input#username").fill(domain); //grabbing the text from page 2 and typing the text in page 1 username feild
+        await page.pause();
+        console.log(await page.locator("input#username").textContent());
 
 });
---
+++ tests/waitConcepts.spec.js
@@ -0,0 +1,30 @@
+const{test,expect} = require('@playwright/test')   //imprting test and expect annotations
+const { AsyncLocalStorage } = require('async_hooks')
+test.only ('wait playwright test',async ({browser,page})=>
+
+{
+    const userName=page.locator("#userEmail");
+    const password= page.locator("#userPassword");
+    const loginbtn= page.locator("#login");
+await page.goto("https://rahulshettyacademy.com/client");
+
+await userName.fill("rachanabanerjee66@gmail.com");
+await password.fill("Delilah@1312");
+await loginbtn.click();
+
+//Eventhough playwright has auto waiting mechanism, it doesnot implement autowaiting in case of "alltextcontents() function. so it will throw error because the
+// alltextcontents() will be called before the page loads all the content
+//to resolve this, we will use the function  waitForLoadState("networkidle").
+await page.waitForLoadState("networkidle"); // this function makes sure all the network calls are made (api service calls) and page is fully loaded, before going to the next step so the all texts contents are fetched without any error
+
+const prodTitle= await page.locator(".card-body b").allTextContents();
+console.log(prodTitle);
+
+//sometimes waitForLoadState("networkidle") is flaky , so if it doesnot work , alternatively we can use another method waitFor() to wait for the element of that CSS to be loaded and then proceed for the next step
+
+await page.locator("section #res").waitFor();// waitFor() will let the test wait till the element is loaded and then proceed for the next step
+                                             // waitFor() will only work for css returning only one element
+
+await page.locator("div button").last().waitFor(); //since waitFor() works only for single element , use last() so that if css has mathcing multiple elements, it will fetch only the last one
+
+});
